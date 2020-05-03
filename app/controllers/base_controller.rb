147d7b25b5fa2028a415003f41d3ffbdd48e48be
Ya-Rong, Teng <c00ooc0207@gmail.com>
2020-05-03 16:40:13 +0800

    feat(controller): add LinksController #index and #show actions

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class BaseController < ApplicationController
+  private
+
+  def set_flash_message(text = 'Process Completedly', type = :notice)
+    flash[type] = text
+  end
 end
